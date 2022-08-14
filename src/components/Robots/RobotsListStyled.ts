9c375d5161791f970655695d1138835c5c18238f
Berta Floran <bertafloran@hotmail.com>
2022-08-14 22:53:18 +0200

    create Robots List component

---
+++ src/components/Robots/RobotsListStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const RobotsListStyled = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
+  .robots-list__title {
+    font-size: 2rem;
+    color: #807776;
+    text-align: center;
+  }
+`;
+
+export default RobotsListStyled;
