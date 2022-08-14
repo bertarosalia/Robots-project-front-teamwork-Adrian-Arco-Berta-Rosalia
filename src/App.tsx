d2efbe82385cbe3b1871410a73cefd40d12da6e1
Adrian Arco <adrian.arco.gonzalez@gmail.com>
2022-08-14 21:59:17 +0200

    deleted card from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import Card from "./components/Card/Card";
-
 const App = (): JSX.Element => {
-  return (
-    <div className="App">
-      <Card
-        urlImage=""
-        creationDate=""
-        endurance={3}
-        name="asd"
-        speed={3}
-        key={"name"}
-      ></Card>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
