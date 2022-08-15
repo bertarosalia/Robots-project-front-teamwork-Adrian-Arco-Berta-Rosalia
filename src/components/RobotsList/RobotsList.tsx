dfaf7e659077cbb228317e4d8dd7cbde6e317f88
Berta Floran <bertafloran@hotmail.com>
2022-08-15 19:13:13 +0200

    add functionality to robots list and style

---
+++ src/components/RobotsList/RobotsList.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../features/store/store";
+import useApi from "../../hooks/useApi";
+import Card from "../Card/Card";
+import RobotsListStyled from "./RobotsListStyled";
+
+const RobotsList = (): JSX.Element => {
+  const { getAll } = useApi();
+  const robots = useSelector((state: RootState) => state.robotsReducer);
+
+  useEffect(() => {
+    getAll();
+  }, [getAll]);
+
+  return (
+    <>
+      <RobotsListStyled>
+        <h1 className="robots-list__title">Robots</h1>
+        <section className="robots-list">
+          {robots.map((robot) => (
+            <Card
+              id={robot.id}
+              urlImage={robot.urlImg}
+              name={robot.name}
+              speed={robot.speed}
+              endurance={robot.endurance}
+              creationDate={robot.creationDate}
+            />
+          ))}
+        </section>
+      </RobotsListStyled>
+    </>
+  );
+};
+
+export default RobotsList;
